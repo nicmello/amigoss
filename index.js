889b800da15cf97401bbcec64938ab8e6760091c
nic <124655178+nicmello@users.noreply.github.com>
2023-08-26 21:02:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let resposta = prompt(
+  "Você é a Maria ou o Guilherme? Responda com 1 para sim ou 2 para não:"
+);
+
+if (resposta === "1") {
+  alert("Vai tomar no cu!");
+} else if (resposta === "2") {
+  alert("Que seu resto de sábado seja excelente! <3");
+} else {
+  alert("Opção inválida. Responda com 1 para sim ou 2 para não.");
+}
